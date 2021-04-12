1aff50868124d4041498393379a30967400e38a9
KG sensei <60828719+kgsensei@users.noreply.github.com>
2021-04-12 17:23:01 -0400

    Fixed stuff, Broke stuff.

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 import pyautogui as pya
 from PIL import ImageGrab
-import pytesseract, cv2, numpy, time, keyboard
-pytesseract.pytesseract.tesseract_cmd = r'tesseract.exe path here'
+import cv2, time
+
 def Tick():
-     obj=False;obd=False
-     image=ImageGrab.grab(bbox=(705,366,868,429))
+     obj=False
+     obd=False
+     image=ImageGrab.grab(bbox=(737,432,875,477))
      image.save("TickScanner.png")
-     if pya.locate("./Danger/A_1.png", "TickScanner.png", confidence=0.7) != None:
+     if pya.locate("./Danger/A_1.png","TickScanner.png",confidence=0.8,grayscale=True)!= None:
           obj=True
-     if pya.locate("./Danger/A_2.png", "TickScanner.png", confidence=0.7) != None:
+     if pya.locate("./Danger/A_2.png","TickScanner.png",confidence=0.8,grayscale=True)!= None:
           obj=True
-     if pya.locate("./Danger/A_3.png", "TickScanner.png", confidence=0.7) != None:
+     if pya.locate("./Danger/A_3.png","TickScanner.png",confidence=0.8,grayscale=True)!= None:
           obj=True
-     if pya.locate("./Danger/A_4.png", "TickScanner.png", confidence=0.7) != None:
+     if pya.locate("./Danger/A_4.png","TickScanner.png",confidence=0.8,grayscale=True)!= None:
           obj=True
-     if pya.locate("./Danger/B_1.png", "TickScanner.png", confidence=0.7) != None:
+     if pya.locate("./Danger/B_1.png","TickScanner.png",confidence=0.8,grayscale=True)!= None:
           obd=True
-     if pya.locate("./Danger/B_2.png", "TickScanner.png", confidence=0.7) != None:
+     if pya.locate("./Danger/B_2.png","TickScanner.png",confidence=0.8,grayscale=True)!= None:
           obd=True
-     if pya.locate("./Danger/B_3.png", "TickScanner.png", confidence=0.7) != None:
+     if pya.locate("./Danger/B_3.png","TickScanner.png",confidence=0.8,grayscale=True)!= None:
           obj=True
-     if pya.locate("./Danger/B_4.png", "TickScanner.png", confidence=0.7) != None:
+     if pya.locate("./Danger/B_4.png","TickScanner.png",confidence=0.8,grayscale=True)!= None:
           obj=True
      if obd == True:
           return "ObD"
      if obj == True:
           return "ObJ"
+
 while True:
      OBJTF=Tick()
      if OBJTF == "ObJ":
@@ -36,4 +38,4 @@ while True:
           print("Object Detected - Duck")
           time.sleep(0.125)
           pya.keyUp('down')
-     time.sleep(0.0005)
+     time.sleep(0.00075)
